9a0131d2ba68d9d1238aeef50b716278da96c4af
Patrick McKenna <patrickbm@github.com>
2015-04-02 14:03:20 -0700

    create/set status for a ref in web UI

---
+++ api/bash/set-status.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+. plumbing.sh
+
+# $1 = username/repo
+# $2 = ref
+# $3 = status info (in JSON format)
+postStatus()
+{
+  callAPI "repos/$1/statuses/$2" -d "$3"
+}
+
+postStatus "$@"
