68a02efcbcfb26236a04b34f733ae0535d8eeb41
Nathan Henderson <nathos@github.com>
2015-05-27 18:26:31 -0400

    Improve token handling; add support for GHE endpoints
    - uses GITHUB_TOKEN and GITHUB_HOSTNAME environment variables
    - improved argument argument & error handling

--- api/ruby/2fa_checker.rb
+++ api/ruby/2fa_checker.rb
@@ -1,16 +1,33 @@
 require 'octokit.rb'
 
-if ARGV.length != 1
-  $stderr.puts "Pass in the name of the organization you're interested in checking."
+begin
+  ACCESS_TOKEN = ENV.fetch("GITHUB_TOKEN")
+  HOSTNAME = ENV.fetch("GITHUB_HOSTNAME")
+rescue KeyError
+  $stderr.puts "To run this script, please set the following environment variables:"
+  $stderr.puts "- GITHUB_TOKEN: A valid access token with Organzation admin priviliges"
+  $stderr.puts "- GITHUB_HOSTNAME: A valid GitHub Enterprise hostname"
   exit 1
 end
 
-# !!! DO NOT EVER USE HARD-CODED VALUES IN A REAL APP !!!
-# Instead, set and test environment variables, like below
-client = Octokit::Client.new(:access_token => ENV['MY_PERSONAL_TOKEN'])
+Octokit.configure do |kit|
+  kit.api_endpoint = "#{HOSTNAME}/api/v3"
+  kit.access_token = ACCESS_TOKEN
+  kit.auto_paginate = true
+end
+
+if ARGV.length != 1
+  $stderr.puts "Pass a valid Organization name to audit."
+  exit 1
+end
 
 ORG = ARGV[0].to_s
 
-client.organization_members(ORG, { :filter => "2fa_disabled" }).each do |user|
-  puts "#{user[:login]} does not have 2FA enabled, and yet is a member of #{ORG}!"
+client = Octokit::Client.new
+
+users = client.organization_members(ORG, {:filter => "2fa_disabled"})
+
+puts "The following #{users.count} users do not have 2FA enabled:\n\n"
+users.each do |user|
+  puts "#{user[:login]}"
 end
