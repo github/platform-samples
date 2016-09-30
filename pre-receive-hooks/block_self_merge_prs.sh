162de68b0bc3615798604c4a4d2eb57a384ee148
Jamie Jones <jbjonesjr@github.com>
2016-09-29 21:50:21 -0400

    create explanatory comment

--- pre-receive-hooks/block_self_merge_prs.sh
+++ pre-receive-hooks/block_self_merge_prs.sh
@@ -1,1 +1,10 @@
+#!/usr/bin/env bash
+
+#
+# Pre-receive hook that will reject all merge attempts
+# of a PR attempted by the author
+#
+# More details on pre-receive hooks and how to apply them can be found on
+# https://help.github.com/enterprise/admin/guides/developer-workflow/managing-pre-receive-hooks-on-the-github-enterprise-appliance/
+#
 
