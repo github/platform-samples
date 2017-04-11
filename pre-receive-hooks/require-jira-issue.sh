59ba5e247cd83b36ef90a02647f6bb1d9126095e
Pavan Ravipati <psravipati@gmail.com>
2017-04-11 16:16:13 -0700

    Add djdefi's version

--- pre-receive-hooks/require-jira-issue.sh
+++ pre-receive-hooks/require-jira-issue.sh
@@ -1,29 +1,19 @@
-#!/usr/bin/env bash
+#!/bin/bash
+#
+# check commit messages for JIRA issue numbers formatted as [JIRA-<issue number>]
 
-zero_commit="0000000000000000000000000000000000000000"
+REGEX="\[JIRA\-[0-9]*\]"
 
-read oldrev newrev refname
-echo $oldrev $newrev $refname
+ERROR_MSG="[POLICY] The commit doesn't reference a JIRA issue"
 
-check_message_format ()
-# enforced custom commit message format
-{
-  message=`git cat-file commit $newrev | sed '1,/^$/d'`
-  regex="/*\[jira-.*\]"
-  echo "[COMMIT MESSAGE]:" $message
-    if [[  $message =~ $regex ]];
-  then
-    echo "Commit message looks good!"
-    exit 0
-  else
-    echo "[POLICY] Commit message does not contain a JIRA ticket #"
-    exit 1
-
-  fi
-}
-
-if [ "$newrev" = "$zero_commit" ]; then
-  continue
-else
-  check_message_format
-fi
+while read OLDREV NEWREV REFNAME ; do
+  for COMMIT in `git rev-list $OLDREV..$NEWREV`;
+  do
+    MESSAGE=`git cat-file commit $COMMIT | sed '1,/^$/d'`
+    if ! echo $MESSAGE | grep -iqE "$REGEX"; then
+      echo "$ERROR_MSG: $MESSAGE" >&2
+      exit 1
+    fi
+  done
+done
+exit 0
