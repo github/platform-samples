ad1146ea620a2ef8b26a2bb9de4a8670ce671fd7
Thomas Osowski <osowskit@github.com>
2017-06-24 07:13:14 -0700

    Simplify to use octokit.rb methods

--- app/ruby/app-issue-creator/server.rb
+++ app/ruby/app-issue-creator/server.rb
@@ -1,10 +1,10 @@
 require 'sinatra'
 require 'jwt'
-require 'rest_client'
 require 'json'
 require 'active_support/all'
 require 'octokit'
 
+@client = nil
 
 post '/payload' do
   github_event = request.env['HTTP_X_GITHUB_EVENT']
@@ -17,8 +17,8 @@ post '/payload' do
 
 end
 
-def get_jwt
-  path_to_pem = './platform-samples.pem'
+def get_jwt_token
+  path_to_pem = './platform-samples-app-bot.2017-06-24.private-key.pem'
   private_pem = File.read(path_to_pem)
   private_key = OpenSSL::PKey::RSA.new(private_pem)
 
@@ -27,69 +27,53 @@ def get_jwt
     iat: Time.now.to_i,
     # JWT expiration time (10 minute maximum)
     exp: 5.minutes.from_now.to_i,
-    # Integration's GitHub identifier
+    # GitHub App's identifier
     iss: 2583
   }
 
   JWT.encode(payload, private_key, "RS256")
 end
 
-def get_app_repositories(token)
-  url = "https://api.github.com/installation/repositories"
-  headers = {
-    authorization: "token #{token}",
-    accept: "application/vnd.github.machine-man-preview+json"
-  }
+def get_app_repositories
 
-  response = RestClient.get(url,headers)
-  json_response = JSON.parse(response)
+  json_response = @client.list_installation_repos
 
   repository_list = []
-  if json_response["total_count"] > 0
+  if json_response.count > 0
     json_response["repositories"].each do |repo|
       repository_list.push(repo["full_name"])
     end
+  else
+    puts json_response
   end
 
   repository_list
 end
 
 
-def create_issues(access_token, repositories, sender_username)
-  client = Octokit::Client.new(access_token: access_token )
-  client.default_media_type = "application/vnd.github.machine-man-preview+json"
-
+def create_issues(repositories, sender_username)
   repositories.each do |repo|
     begin
-      client.create_issue(repo, "#{sender_username} created new app!", "Added GitHub App")
+      @client.create_issue(repo, "#{sender_username} created new app!", "Added GitHub App")
     rescue
-      puts "no issues in this repository"
+      puts "Issues is disabled for this repository"
     end
   end
 end
 
-
-def get_app_token(access_tokens_url)
-  jwt = get_jwt
-
-  headers = {
-    authorization: "Bearer #{jwt}",
-    accept: "application/vnd.github.machine-man-preview+json"
-  }
-  response = RestClient.post(access_tokens_url,{},headers)
-
-  app_token = JSON.parse(response)
-  app_token["token"]
-end
-
-
 def parse_installation_payload(json_body)
   webhook_data = JSON.parse(json_body)
   if webhook_data["action"] == "created" || webhook_data["action"] == "added"
-    access_tokens_url = webhook_data["installation"]["access_tokens_url"]
+    installation_id = webhook_data["installation"]["id"]
     # Get token for app
-    app_token = get_app_token(access_tokens_url)
-    
+    puts get_jwt_token
+    jwt_client = Octokit::Client.new(:bearer_token => get_jwt_token)
+    jwt_client.default_media_type = "application/vnd.github.machine-man-preview+json"
+    app_token = jwt_client.create_installation_access_token(installation_id)
+
+    @client = Octokit::Client.new(access_token: app_token[:token] )
+    @client.default_media_type = "application/vnd.github.machine-man-preview+json"
+        
     repository_list = []
     if webhook_data["installation"].key?("repositories_added")
       webhook_data["installation"]["repositories_added"].each do |repo|
@@ -97,9 +81,9 @@ def parse_installation_payload(json_body)
       end
     else
       # Get repositories by query
-      repository_list = get_app_repositories(app_token) 
+      repository_list = get_app_repositories
     end
     
-    create_issues(app_token, repository_list, webhook_data["sender"]["login"])
+    create_issues(repository_list, webhook_data["sender"]["login"])
   end
 end
