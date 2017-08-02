61cf9256188ea29549a4fa2ffc812fca7d99e76f
Daniel Figucio <affrae@users.noreply.github.com>
2017-08-03 05:57:41 +1000

    comma fix

--- pre-receive-hooks/block_branch_names_not_starting_with_userID.sh
+++ pre-receive-hooks/block_branch_names_not_starting_with_userID.sh
@@ -21,7 +21,9 @@ while read oldrev newrev refname; do
     fi
   fi
 done
-echo "Hi, $GITHUB_USER_LOGIN allowing creation of new branch $refname"
+# The following echos may be removed if you like . It's purely a cosmetic demo item
+# To show that it does not have to be just error messages passed back.
+echo "Hi $GITHUB_USER_LOGIN, allowing creation of new branch $refname"
 echo "because it does start with your username ($GITHUB_USER_LOGIN)"
 echo "as outlined in the branch naming policy guide - thank you!"
 exit 0
