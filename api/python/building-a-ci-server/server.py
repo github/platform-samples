00008ec13d7a446065ea3c10d3a4333ddebb6527
Wan Liuyang <tsfdye@gmail.com>
2017-10-14 22:52:05 +0800

    Fix Python implementation of simple ci server
    - Update pyramid version to 1.9.1
    - Update in favor of new GitHub API

--- api/python/building-a-ci-server/server.py
+++ api/python/building-a-ci-server/server.py
@@ -3,6 +3,7 @@ from __future__ import print_function
 from wsgiref.simple_server import make_server
 from pyramid.config import Configurator
 from pyramid.view import view_config, view_defaults
+from pyramid.response import Response
 
 
 @view_defaults(
@@ -23,29 +24,25 @@ class PayloadView(object):
     def payload_push(self):
         """This method is a continuation of PayloadView process, triggered if
         header HTTP-X-Github-Event type is Push"""
-        # {u'name': u'marioidival', u'email': u'marioidival@gmail.com'}
         print(self.payload['pusher'])
 
         # do busy work...
-        return "nothing to push payload" # or simple {}
+        return "nothing to push payload"  # or simple {}
 
     @view_config(header="X-Github-Event:pull_request")
     def payload_pull_request(self):
         """This method is a continuation of PayloadView process, triggered if
         header HTTP-X-Github-Event type is Pull Request"""
-        # {u'name': u'marioidival', u'email': u'marioidival@gmail.com'}
-        print(self.payload['pusher'])
+        print(self.payload['sender'])
 
         # do busy work...
-        return "nothing to pull request payload" # or simple {}
+        return "nothing to pull request payload"  # or simple {}
 
 
 if __name__ == "__main__":
-    config = Configurator()
-
-    config.add_route("github_payload", "/github_payload/")
-    config.scan()
-
-    app = config.make_wsgi_app()
-    server = make_server("0.0.0.0", 8888, app)
+    with Configurator() as config:
+        config.add_route('github_payload', '/github_payload')
+        config.scan()
+        app = config.make_wsgi_app()
+    server = make_server('0.0.0.0', 6543, app)
     server.serve_forever()
