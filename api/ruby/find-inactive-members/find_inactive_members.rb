0bae249244a38dc1a54a05f129dd6b003bdd4ee0
Asendia Mayco <asendia@traveloka.com>
2019-11-09 18:30:31 +0700

    Remove empty spaces

--- api/ruby/find-inactive-members/find_inactive_members.rb
+++ api/ruby/find-inactive-members/find_inactive_members.rb
@@ -98,7 +98,7 @@ private
         active: false
       }
     end
-    
+
     info "#{@members.length} members found.\n"
   end
 
@@ -286,12 +286,12 @@ private
     # if there is no following period that is followed by something
     # other than a period (yeah, confusing, I know)
     fn = filename.split /(?<=.)\.(?=[^.])(?!.*\.[^.])/m
-  
+
     # We now have one or two parts (depending on whether we could find
     # a suitable period). For each of these parts, replace any unwanted
     # sequence of characters with an underscore
     fn.map! { |s| s.gsub /[^a-z0-9\-]+/i, '_' }
-  
+
     # Finally, join the parts with a period and return the result
     return fn.join '.'
   end
