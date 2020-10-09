0e0be86929e4f1e90acba0fb8cc89cfb4683e451
Prem Kumar Ponuthorai <ppremk@github.com>
2020-10-09 13:55:09 +0200

    code review fix

--- api/bash/create-teams.sh
+++ api/bash/create-teams.sh
@@ -13,28 +13,31 @@
 # - ./script.sh
 
 # VARS
-ORGNAME="xxxx" 
-PATTOKEN="xxxx"
-TEAMS=("team-name-1" "team-name-2")
+orgname="xxx" 
+pattoken="xxxxxxx"
+teams=("team-name-1" "team-name-2")
 
 echo "Bulk creating teams in:"
-echo $ORGNAME
+echo $orgname
 
-for i in "${TEAMS[@]}"
-do
-curl --request POST \
-  --url "https://api.github.com/orgs/$ORGNAME/teams" \
-  --header "accept: application/vnd.github.v3+json" \
-  --header "authorization: Bearer ${PATTOKEN}" \
-  --header "content-type: application/json" \
-  --data "{\"name\": \"$i\", \"privacy\": \"closed\" }"
+for i in "${teams[@]}"
+  do
+    curl --request POST \
+      --url "https://api.github.com/orgs/$orgname/teams" \
+      --header "accept: application/vnd.github.v3+json" \
+      --header "authorization: Bearer ${pattoken}" \
+      --header "content-type: application/json" \
+      --data "{\"name\": \"$i\", \"privacy\": \"closed\" }" \
+      -- fail
+
+    retVal=$?
+    if [ $retVal -ne 0 ]; then
+      echo "Team creation failed! Please verify validity of supplied configurations."
+      exit 1
+    fi  
 done
+echo "Teams succesfully created!"
 
-if [ $ERROR_CODE -ne 0 ]; then
-  echo "Team creation failed! Please verify validity of supplied configurations."
-  exit 1
-else
-  echo "Teams succesfully created!"
-fi
+
 
 
