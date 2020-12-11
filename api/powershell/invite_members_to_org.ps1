688f2749c78534f2ec3a4ee1188a5226570d4615
Ramon da Silva <radasi@microsoft.com>
2020-12-11 13:05:53 -0300

    Break some lines

--- api/powershell/invite_members_to_org.ps1
+++ api/powershell/invite_members_to_org.ps1
@@ -5,9 +5,13 @@
 Invites GitHub Enterprise members to an organization
 
 .DESCRIPTION
-This script runs a batch organization invitation process for a given list of GitHub enterprise consumed licenses.
+This script runs a batch organization invitation process for a given list of GitHub enterprise
+consumed licenses.
 
-The input is a CSV file with a column named "Handle or email", such as can be exported from the Enterprise settings > Enterprise licensing page. Users with appropriate permissions can export the CSV file, edit it in their favorite spreadsheet to select emails to invite, then use this script to invite them to an org.
+The input is a CSV file with a column named "Handle or email", such as can be exported from the
+Enterprise settings > Enterprise licensing page. Users with appropriate permissions can export
+the CSV file, edit it in their favorite spreadsheet to select emails to invite, then use this
+script to invite them to an org.
 
 .PARAMETER LicensesFile
 The path of the consumed licenses CSV.
