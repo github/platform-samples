e023d59f6b35d685f074ab31deb8e5783c21500b
Ramon Bispo <ramonpage@github.com>
2020-12-16 16:57:43 -0300

    Update script description
    Make it clear that the script is for Enterprise Cloud customers

--- api/powershell/invite_members_to_org.ps1
+++ api/powershell/invite_members_to_org.ps1
@@ -5,8 +5,8 @@
 Batch invite members to an organization
 
 .DESCRIPTION
-This script runs a batch organization invitation process for a given list of GitHub enterprise
-consumed licenses.
+This script runs a batch organization invitation process for a given list of GitHub Enterprise
+Cloud consumed licenses.
 
 The input is a CSV file with a column named "Handle or email", such as can be exported from the
 Enterprise settings > Enterprise licensing page. Users with appropriate permissions can export
