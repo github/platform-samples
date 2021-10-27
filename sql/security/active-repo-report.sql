790203734a387a6ec42dd5e3d7a4a9cc19c35248
Natalie Somersall <Somersall_Natalie@bah.com>
2021-10-26 20:45:07 -0600

    add issue created summary

--- sql/security/active-repo-report.sql
+++ sql/security/active-repo-report.sql
@@ -3,7 +3,7 @@
  * repositories pushed to in the past 90 days.  It also returns who owns it and
  * further details on the exact vulnerability.
  *
- * If you comment line 34, it will both root and fork repositories.  As is, 
+ * If you comment line 32, it will both root and fork repositories.  As is, 
  * it will only report root repos.
  */
 SELECT
